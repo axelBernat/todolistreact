235df5e2636067c696e9c89febe4be5ca33416b6
Axel B <axelrohrig@gmail.com>
2021-03-04 10:47:25 +0100

    init

---
+++ src/Todo.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+
+export default class Todo extends Component {
+    state = {
+        element: '',
+        items: []
+    }
+    onChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault()
+        this.setState({
+            element: '',
+            items : [...this.state.items, {element: this.state.element}]
+
+        })
+    }
+    deleteItem = (index) => {
+        const arr = this.state.items
+        arr.splice(index,1)
+        this.setState({
+            items: arr
+        })
+    }
+    renderTodo = () => {
+        return this.state.items.map((item,index) => {
+            return (
+                item.element !== '' ? (
+                <div className="card mb-3" key={index}>
+                <div className="card-body">
+                    <h4 className="d-flex justify-content-between align-items-center">{item.element}
+                    <i onClick={() => this.deleteItem(index)} className="fas fa-times-circle"
+                    style={{cursor:'pointer',float:'right',color:'red'}}></i>
+                    </h4>
+                </div>
+                </div>
+                ) : null
+            )
+        })
+    }
+    render() {
+        return (
+            <>
+            <div className="card my-3">
+
+                <div className="card-header">To-Do List</div>
+                <div className="card-body">
+                <form onSubmit={this.onSubmit}>
+
+                <div className="form-group">
+                    <label htmlFor="element">Chose à faire</label>
+                    <input type="text" 
+                    className="form-control form-control-lg"
+                    name="element"
+                    onChange={this.onChange}
+                    value={this.state.element}
+                    />
+                </div>
+                <button className="btn btn-primary btn-block">Ajouter une chose à faire</button>
+                </form>
+                </div>
+                </div>
+                {this.renderTodo()}
+            </>
+        )
+    }
+}
